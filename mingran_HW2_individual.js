96fe09203df809fcf2b5a13aa335e8db8ba70a3b
mingran <li1940@purdue.edu>
2015-02-11 12:19:32 -0500

    update

--- mingran_HW2_individual.js
+++ mingran_HW2_individual.js
@@ -1,11 +1,5 @@
-<%@LANGUAGE="JAVASCRIPT" CODEPAGE="65001"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>mingran_HW2</title>
 
- $(document).ready(function () {
+    $(document).ready(function () {
        $.getJSON("https://hivelab.org/static/students.json", function (json) {
         var tr;
         for (var i = 0; i < json.length; i++) {
@@ -17,10 +11,7 @@
             tr.append("<td>" + json[i].Essay+ "</td>");
             tr.append("<td>" + json[i].Recom+ "</td>");
             $('#hw2table').append(tr); 
-	});
-
-</head>
-
-<body>
-</body>
-</html>
+        }
+      
+		});
+	});// JavaScript Document
